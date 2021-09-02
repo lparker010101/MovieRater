ae41a965f67235bcee86660f65ab9a8b92386219
Robert <robertdvelasquez72@gmail.com>
2021-09-02 16:57:29 -0400

    Added Movie and Tv Show services class. This is to prevent merge conflicts when we start working on the services that way everyone has a services class already

---
+++ MovieRater.Services/MovieServices/MovieService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRater.Services.MovieServices
+{
+    public class MovieService
+    {
+        private readonly Guid _id; // making an _id of type Guid
+
+        public MovieService(Guid id)
+        {
+            _id = id; // takes in an id of type Guid and sets it = to our _id to be used 
+        }
+    }
+}
