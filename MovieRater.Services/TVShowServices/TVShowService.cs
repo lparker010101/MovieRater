deb73bb2791f1087ee4e82b1931a5bc99da7db70
Robert <robertdvelasquez72@gmail.com>
2021-09-04 10:26:30 -0400

    Added Tv show service delete and controller

--- MovieRater.Services/TVShowServices/TVShowService.cs
+++ MovieRater.Services/TVShowServices/TVShowService.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using MovieRater.Data;
+using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +9,25 @@ namespace MovieRater.Services.TVShowServices
 {
     public class TVShowService
     {
-        // Get
-        // Post
-        private readonly Guid _id;
-
+        private readonly Guid _Id;
         public TVShowService(Guid id)
         {
-            _id = id;
+            _Id = id;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                TVShow tvShow = await ctx.TVShows.FindAsync(id);
+                if (tvShow is null)
+                {
+                    return false;
+                }
+
+                ctx.TVShows.Remove(tvShow);
+                return await ctx.SaveChangesAsync() > 1;
+            }
         }
     }
-
 }
-
-  
-
