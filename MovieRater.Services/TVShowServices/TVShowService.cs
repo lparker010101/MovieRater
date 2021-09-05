2f166ed37093582b2d7a4f14dc101da94f867a51
Laura Parker <laura.parker03@yahoo.com>
2021-09-05 13:10:27 -0400

    Parker's Last Commit

--- MovieRater.Services/TVShowServices/TVShowService.cs
+++ MovieRater.Services/TVShowServices/TVShowService.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +8,242 @@ namespace MovieRater.Services.TVShowServices
 {
     public class TVShowService
     {
-        // Get
-        // Post
-        private readonly Guid _id;
-
+        private readonly Guid _Id;
         public TVShowService(Guid id)
         {
-            _id = id;
+            _Id = id;
+        }
+
+        public async Task<bool> CreateTVShow(TVShowCreate model)
+        {
+            var entity =
+                new TVShow() // This creates an instance of TVShow.
+                {
+                    Title = model.Title,
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.TVShows.Add(entity);
+                return await ctx.SaveChangesAsync() == 1;
+            }
+        }
+
+        public async Task<IEnumerable<TVShowListItem>> GetTVShows()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    await
+                    ctx
+                    .TVShows
+                    .Select(c => new TVShowListItem
+                    {
+                        Id = c.Id,
+                        Title = c.Title,
+                        ReleaseDate = c.ReleaseDate,
+                        Genre = c.Genre,
+                        Description = c.Description,
+                    }).ToListAsync();
+                return query;
+            }
+        }
+
+        public async Task<TVShowDetail> GetTVShowById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var show =
+                    await
+                    ctx
+                    .TVShows
+                    .Where(c => c.Id == id)
+                    .SingleOrDefaultAsync(c => c.Id == id);
+                if (show is null)
+                {
+                    return null;
+                }
+
+                return new TVShowDetail
+                {
+                    Id = show.Id,
+                    Title = show.Title,
+                    ReleaseDate = show.ReleaseDate,
+                    Genre = show.Genre,
+                    Description = show.Description,
+                    MainCharacters = show.MainCharacters,
+                };
+            }
         }
     }
-
 }
+                    Title = model.Title,
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.TVShows.Add(entity);
+                return await ctx.SaveChangesAsync() == 1;
+            }
+        }
 
-  
+        public async Task<IEnumerable<TVShowListItem>> GetTVShows()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    await
+                    ctx
+                    .TVShows
+                    .Select(c => new TVShowListItem
+                    {
+                        Id = c.Id,
+                        Title = c.Title,
+                        ReleaseDate = c.ReleaseDate,
+                        Genre = c.Genre,
+                        Description = c.Description,
+                    }).ToListAsync();
+                return query;
+            }
+        }
 
+        public async Task<TVShowDetail> GetTVShowById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var show =
+                    await
+                    ctx
+                    .TVShows
+                    .Where(c => c.Id == id)
+                    .SingleOrDefaultAsync(c => c.Id == id);
+                if (show is null)
+                {
+                    return null;
+                }
+
+                return new TVShowDetail
+                {
+                    Id = show.Id,
+                    Title = show.Title,
+                    ReleaseDate = show.ReleaseDate,
+                    Genre = show.Genre,
+                    Description = show.Description,
+                    MainCharacters = show.MainCharacters,
+                };
+            }
+        }
+    }
+}
+                    Title = model.Title,
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.TVShows.Add(entity);
+                return await ctx.SaveChangesAsync() == 1;
+            }
+        }
+
+        public async Task<IEnumerable<TVShowListItem>> GetTVShows()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    await
+                    ctx
+                    .TVShows
+                    .Select(c => new TVShowListItem
+                    {
+                        Id = c.Id,
+                        Title = c.Title,
+                        ReleaseDate = c.ReleaseDate,
+                        Genre = c.Genre,
+                        Description = c.Description,
+                    }).ToListAsync();
+                return query;
+            }
+        }
+
+        public async Task<TVShowDetail> GetTVShowById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var show =
+                    await
+                    ctx
+                    .TVShows
+                    .Where(c => c.Id == id)
+                    .SingleOrDefaultAsync(c => c.Id == id);
+                if (show is null)
+                {
+                    return null;
+                }
+
+                return new TVShowDetail
+                {
+                    Id = show.Id,
+                    Title = show.Title,
+                    ReleaseDate = show.ReleaseDate,
+                    Genre = show.Genre,
+                    Description = show.Description,
+                    MainCharacters = show.MainCharacters,
+                };
+            }
+        }
+    }
+}
+                    Title = model.Title,
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.TVShows.Add(entity);
+                return await ctx.SaveChangesAsync() == 1;
+            }
+        }
+
+        public async Task<IEnumerable<TVShowListItem>> GetTVShows()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    await
+                    ctx
+                    .TVShows
+                    .Select(c => new TVShowListItem
+                    {
+                        Id = c.Id,
+                        Title = c.Title,
+                        ReleaseDate = c.ReleaseDate,
+                        Genre = c.Genre,
+                        Description = c.Description,
+                    }).ToListAsync();
+                return query;
+            }
+        }
+
+        public async Task<TVShowDetail> GetTVShowById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var show =
+                    await
+                    ctx
+                    .TVShows
+                    .Where(c => c.Id == id)
+                    .SingleOrDefaultAsync(c => c.Id == id);
+                if (show is null)
+                {
+                    return null;
+                }
+
+                return new TVShowDetail
+                {
+                    Id = show.Id,
+                    Title = show.Title,
+                    ReleaseDate = show.ReleaseDate,
+                    Genre = show.Genre,
+                    Description = show.Description,
+                    MainCharacters = show.MainCharacters,
+                };
+            }
+        }
+    }
+}
