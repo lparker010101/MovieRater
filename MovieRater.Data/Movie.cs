05f2de296e08584bc589529f112e27f29e338dae
Samir Omerani <omegraph@gmail.com>
2021-09-07 18:14:12 -0400

    My Changes

--- MovieRater.Data/Movie.cs
+++ MovieRater.Data/Movie.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace MovieRater.Data
 {
+
     public class Movie
     {
         [Key]
--- MovieRater.Models/TVShowModels/TVShowCreate.cs
+++ MovieRater.Models/TVShowModels/TVShowCreate.cs
@@ -1,22 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MovieRater.Models.MovieModels
+namespace MovieRater.Models.TVShowModels
 {
-
-    public class MovieEdit
+    public class TVShowCreate
     {
-        public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
-        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public int ReleaseDate { get; set; }
+        [Required]
         public string ParentalGuidance { get; set; }
+        [Required]
         public string Genre { get; set; }
+        [Required]
         public double Rating { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string MainCharacters { get; set; }
-        public List<string> PlacesToWatch { get; set; } = new List<string>();
+        [Required]
+        public string PlacesToWatch { get; set; }
+        public int Seasons { get; set; }
     }
 }
